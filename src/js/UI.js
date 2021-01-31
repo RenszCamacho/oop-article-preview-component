755b10fc51767e58b5cc5d747f6c4a3c236240cb
Renszo Camacho <varsovia84@gmail.com>
2021-01-31 21:06:46 +0100

    handleClick method. Done.

--- src/js/UI.js
+++ src/js/UI.js
@@ -84,4 +84,18 @@ class UI {
     `;
     root.appendChild(article);
   }
+
+  handleClick() {
+    const cardBtn = document.getElementById("card__btn");
+
+    const toogle = (btn) => {
+      const active = document.getElementById("active");
+      btn.addEventListener("click", () => {
+        active.classList.toggle("active");
+        cardBtn.classList.toggle("card__btn--active");
+      });
+    };
+
+    toogle(cardBtn);
+  }
 }

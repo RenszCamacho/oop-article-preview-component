d4b0ec5bb9cd47f1323c6f1bf56b986147dd308d
Renszo Camacho <varsovia84@gmail.com>
2021-01-31 20:45:57 +0100

    data.js Done.

---
+++ src/js/data.js
@@ -0,0 +1,15 @@
+import poster from "../images/drawers.jpg";
+import avatar from "../images/avatar-michelle.jpg";
+
+const data = {
+  poster: poster,
+  title:
+    "Shift the overall look and feel by adding these wonderful touches to furniture in your home",
+  description:
+    "Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.",
+  image: avatar,
+  name: "Michelle Appleton",
+  date: "28 Jun 2020",
+};
+
+export default data;
